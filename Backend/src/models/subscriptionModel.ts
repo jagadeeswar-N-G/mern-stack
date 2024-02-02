98a4e45968bdc0a9b3cf2429752ee90e632a5499
jagadeeswar-N-G <jagadeeswar8055@gmail.com>
2024-02-02 13:25:09 +0530

    subcription model

---
+++ Backend/src/models/subscriptionModel.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionModel = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscriber = mongoose.model("Subscriber", subscriptionModel);
